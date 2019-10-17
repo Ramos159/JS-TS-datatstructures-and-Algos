0efc2c955f61f73f7e5d7e933438bc8ad7f151e5
Ramos159 <edwinramos269@gmail.com>
2019-10-17 03:52:32 -0400

    moved single list node tests to the regular list tests

--- tests/singlelinkedlist.test.ts
+++ tests/singlelinkedlist.test.ts
@@ -16,6 +16,16 @@ describe('SinglyLinkedList',()=>{
             expect(list.getSize()).toEqual(0)
         })
     })
+    describe('Single List Node',()=>{
+        test('has a value property',()=>{
+            let node: SingleListNode = new SingleListNode(1)
+            expect(node).toMatchObject({value:1})
+        })
+        test('has a next property that is intialized to null',()=>{
+            let node: SingleListNode = new SingleListNode(1)
+            expect(node).toMatchObject({next:null})
+        })
+    })
     describe('addToFrontOfList',()=>{
         test('inserts nodes to the front correctly',()=>{
             let list: SinglyLinkedList = new SinglyLinkedList()
