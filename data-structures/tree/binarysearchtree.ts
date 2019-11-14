659b1cdfe6f180e3c131b3153353df21ecdae3fb
Ramos159 <edwinramos269@gmail.com>
2019-11-14 00:52:01 -0500

    working tree with insert

--- data-structures/tree/binarysearchtree.ts
+++ data-structures/tree/binarysearchtree.ts
@@ -1,11 +1,56 @@
-import BinarySearchTreeNode as Node from './BinarySearchTreeNode'
+import Node from './binarysearchtreenode'
 
 export default class BinarySearchTree {
-    public root: Node | null
+    private root: Node | null
 
     public constructor(){
         this.root = null
     }
 
-    
+    public getRoot = () => {
+        return this.root
+    }
+
+    public insert = (value: number): void => {
+        const node: Node = new Node(value)
+
+        if(!this.root){
+            this.root = node
+        }
+        else if(this.root.value > node.value){
+            if(this.root.left){
+                return this.insertTraverse(this.root.left,node)
+            }
+            else{
+                this.root.left = node
+            }
+        }
+        else if(this.root.value < node.value){
+            if(this.root.right){
+                return this.insertTraverse(this.root.right,node)
+            }
+            else{
+                this.root.right = node
+            }
+        }
+    }
+
+    private insertTraverse = (root: Node, value: Node): void => {
+        if(root.value > value.value){
+            if(root.left){
+                return this.insertTraverse(root.left,value)
+            }
+            else{
+                root.left = value
+            }
+        }
+        else{
+            if(root.right){
+                return this.insertTraverse(root.right,value)
+            }
+            else{
+                root.right = value
+            }
+        }
+    }
 }
--- tests/binarysearchtree.test.ts
+++ tests/binarysearchtree.test.ts
@@ -2,6 +2,14 @@ import BinarySearchTree from '../data-structures/tree/BinarySearchTree'
 
 let BST: BinarySearchTree = new BinarySearchTree
 
+beforeEach(()=>{
+    BST = new BinarySearchTree
+})
+
 describe('Binary Search Tree',()=>{
-    
+    describe('Initialization',()=>{
+        test('Tree starts with a empty root',()=>{
+            expect(BST.getRoot()).toEqual(null)
+        })
+    })
 })
