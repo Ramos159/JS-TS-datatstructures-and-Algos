8ab07e7009679b6b25bb6aa42c9c769c146989a3
Ramos159 <edwinramos269@gmail.com>
2019-11-15 00:59:48 -0500

    basic tree and testing file done, maybe ill move on

--- data-structures/tree/binarysearchtree.ts
+++ data-structures/tree/binarysearchtree.ts
@@ -7,15 +7,16 @@ export default class BinarySearchTree {
         this.root = null
     }
 
-    public getRoot = () => {
+    public getRoot = (): Node => {
         return this.root
     }
 
-    public insert = (value: number): void => {
+    public insert = (value: number): boolean => {
         const node: Node = new Node(value)
 
         if(!this.root){
             this.root = node
+            return true
         }
         else if(this.root.value > node.value){
             if(this.root.left){
@@ -23,6 +24,7 @@ export default class BinarySearchTree {
             }
             else{
                 this.root.left = node
+                return true
             }
         }
         else if(this.root.value < node.value){
@@ -31,25 +33,56 @@ export default class BinarySearchTree {
             }
             else{
                 this.root.right = node
+                return true
             }
+        }
+        else if(this.root.value === node.value){
+            return false
         }
     }
 
-    private insertTraverse = (root: Node, value: Node): void => {
-        if(root.value > value.value){
+    private insertTraverse = (root: Node, node: Node): boolean => {
+        if(root.value > node.value){
             if(root.left){
-                return this.insertTraverse(root.left,value)
+                return this.insertTraverse(root.left,node)
             }
             else{
-                root.left = value
+                root.left = node
+                return true
             }
         }
-        else{
+        else if(root.value < node.value){
             if(root.right){
-                return this.insertTraverse(root.right,value)
+                return this.insertTraverse(root.right,node)
             }
             else{
-                root.right = value
+                root.right = node
+                return true
+            }
+        }
+        else if(root.value === node.value){
+            return false
+        }
+    }
+
+    public includes = (value: number,node: Node = this.root): boolean => {
+        if(node.value === value){
+            return true
+        }
+        else if(node.value > value){
+            if(node.left){
+                return this.includes(value,node.left)
+            }
+            else{
+                return false 
+            }
+        }
+        else if(node.value < value){
+            if(node.right){
+                return this.includes(value,node.right)
+            }
+            else{
+                return false
             }
         }
     }
--- tests/binarysearchtree.test.ts
+++ tests/binarysearchtree.test.ts
@@ -2,6 +2,8 @@ import BinarySearchTree from '../data-structures/tree/BinarySearchTree'
 
 let BST: BinarySearchTree = new BinarySearchTree
 
+//testing notes: I Know the tree inserts properly, but im not sure how i would test if its correct
+
 beforeEach(()=>{
     BST = new BinarySearchTree
 })
@@ -10,6 +12,26 @@ describe('Binary Search Tree',()=>{
     describe('Initialization',()=>{
         test('Tree starts with a empty root',()=>{
             expect(BST.getRoot()).toEqual(null)
+        })
+    })
+    describe('insert()',()=>{
+        test('Returns true on a successful insertion of non-duplicate',()=>{
+            expect(BST.insert(1)).toEqual(true)
+            expect(BST.insert(2)).toEqual(true)
+        })
+        test('Returns false when trying to insert a duplicate number',()=>{
+            BST.insert(1)
+            expect(BST.insert(1)).toEqual(false)
+        })
+    })
+    describe('includes()',()=>{
+        test('Returns true when number parameter is found in the tree',()=>{
+            BST.insert(1)
+            expect(BST.includes(1)).toEqual(true)
+        })
+        test('Returns false when number parameter is not found in the tree',()=>{
+            BST.insert(2)
+            expect(BST.includes(3))
         })
     })
 })
